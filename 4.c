b0bed9c2196463c769dfcce2090e2513e4b6e04b
keon-y <63825463+keon-y@users.noreply.github.com>
2023-08-10 15:35:14 -0400

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,558 @@
+// AUTOR: Victor Keony Okabayashi Takatu
+
+#include "gfx/gfx.h"
+#include <stdio.h>  /* printf */
+#include <SDL/SDL.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define WIDTH 1024
+#define HEIGHT 768
+
+
+
+struct s_arq_no {
+    int32_t chave:28;
+    int32_t bal:2;
+    uint32_t esq:1;
+    uint32_t dir:1;
+};
+
+typedef struct s_no {
+    int32_t chave:28;
+    int32_t bal:2;
+    int32_t reservado:2; /* sem uso */
+    struct s_no* esq;
+    struct s_no* dir;
+} Node;
+
+//operacoes
+int search (int, Node*, Node**);
+void insert(int, Node**, int*);
+Node *create(int);
+void desalocateTree(Node*);
+void removeNode(Node*, Node**, Node*, int*);
+
+//getters
+Node *getAncestor(Node*, Node*);
+Node *getMin(Node*);
+Node *getMax(Node*);
+Node *getSuccessor(Node*, int);
+Node *getPredecessor(Node*, int);
+
+//utils
+void paintNode(Node*, int, int, int);
+void pause();
+
+//manipulacao de arquivos
+void readTree(Node**, char*);
+void preorderRead(Node*, FILE*, int);
+void preorderSave(Node*, FILE*);
+void saveTree(Node*, char*);
+
+int main() {
+	
+	Node *root = NULL;
+	char str[30];
+	int h = 0;
+
+	gfx_init(WIDTH, HEIGHT, "");
+
+	int escolha, val;
+	Node *ptr;
+
+	insert(0, &root, &h);
+	insert(-50, &root, &h);
+	insert(50, &root, &h);
+	insert(-100, &root, &h);
+	insert(-25, &root, &h);
+	insert(40, &root, &h);
+	insert(100, &root, &h);
+	insert(-75, &root, &h);
+	insert(30, &root, &h);
+	insert(45, &root, &h);
+	insert(150, &root, &h);
+	insert(20, &root, &h);
+
+	do {
+		gfx_clear();
+		paintNode(root, WIDTH/2, 90, 1);
+		gfx_paint();
+		printf("1. Inserir\n2. Remover\n3. Buscar maior e menor\n4. Buscar sucessor e predecessor\n5. Ler arquivo\n6. Salvar arquivo\n7. Sair\n--> ");
+		scanf("%d", &escolha);
+		switch (escolha){
+			case 1: //insercao
+				printf("\nInforme o valor a ser inserido na arvore: \n --> ");
+				scanf("%d", &val);
+				insert(val, &root, &h);
+			break;
+
+			case 2: //remocao
+				printf("\nInforme o valor a ser removido da arvore:\n --> ");
+				scanf("%d", &val);
+				if (search(val, root, &ptr) == 1) {
+					removeNode(ptr, &root, NULL, &h);
+					printf("[SUCESSO] Elemento %d removido da arvore\n", val);
+				}
+				else printf("[ERRO] Elemento %d nao existe na arvore\n", val);
+			break;
+
+			case 3:
+				ptr = getMax(root);
+				if (ptr)
+					printf("\nMaior valor da arvore: %d\n", ptr->chave);
+				ptr = getMin(root);
+				if (ptr)
+					printf("Menor valor da arvore: %d\n\n", ptr->chave);
+				else printf("[ERRO] Arvore vazia\n");
+			break;
+
+			case 4:
+				printf("\nInforme o valor para descobrir seu sucessor e predecessor: ");
+				scanf("%d", &val);
+				if (search(val, root, &ptr) != 1) {
+					printf("[ERRO] Elemento %d nao existe na arvore", val);
+					
+				} else {
+					ptr = getSuccessor(root, val);
+					if (ptr) printf("Sucessor de %d: %d\n", val, ptr->chave);
+					else printf("[ERRO] %d nao possui sucessor\n", val);
+
+					ptr = getPredecessor(root, val);
+					if (ptr) printf("Predecessor de %d: %d\n", val, ptr->chave);
+					else printf("[ERRO] %d nao possui predecessor\n", val);
+				}
+			break;
+
+			case 5:
+				printf("\nInforme o nome do arquivo:\n --> ");
+				scanf("%s", str);
+				readTree(&root, str);
+			break;
+
+			case 6:
+				printf("\nInforme o nome do arquivo:\n --> ");
+				scanf("%s", str);
+				saveTree(root, str);
+			break;
+
+			case 7:
+			break;
+
+			default:
+				printf("[ERRO] Opcao inexistente");
+		}
+		printf("\n\n");
+		if (escolha != 7) //nao pedir para pressionar tecla quando for sair
+			pause();
+	} while (escolha != 7);
+	gfx_quit();
+	if (root)
+		desalocateTree(root);
+	
+
+	return 1;
+}
+
+
+/*  Procura um valor val e armazena o ponteiro para o no em ret
+	retorna um valor f:
+	0 para arvore vazia
+	1 para elemento encontrado
+	2 para val MENOR que a chave do no
+	3 para val MAIOR que a chave do no
+*/
+int search(int val, Node *node, Node **ret) {
+	if (!node) return 0;
+	*ret = node;
+
+	if (node->chave == val) return 1; //encontrado o elemento exato
+
+	if (val < node->chave && node->esq == NULL) return 2; //menor elemento
+	else if (val < node->chave && node->esq) return search(val, node->esq, ret); //buscar o menor
+
+	if (val > node->chave && node->dir == NULL) return 3;
+	
+	else if (val > node->chave && node->dir) return search(val, node->dir, ret);
+
+	return 0;
+}
+
+//cria um no com chave `chave`
+Node *create(int chave){
+	Node *newNode = malloc(sizeof(Node));
+	newNode->chave = chave;
+	newNode->esq = NULL;
+	newNode->dir = NULL;
+	newNode->bal = 0;
+	return newNode;
+}
+
+void rotateRight(Node **pt) {
+	Node *ptu = (*pt)->esq;
+	if (ptu->bal == -1) {
+		(*pt)->esq = ptu->dir;
+		ptu->dir = *pt;
+		(*pt)->bal = 0;
+		*pt = ptu;
+	}
+	else {
+		Node *ptv = ptu->dir;
+		ptu->dir = ptv->esq;
+		ptv->esq = ptu;
+		(*pt)->esq = ptv->dir;
+		ptv->dir = *pt;
+		if (ptv->bal == -1) (*pt)->bal = 1; 
+			else (*pt)->bal = 0;
+		if (ptv->bal == 1) ptu->bal = -1;
+			else ptu->bal = 0;
+		*pt = ptv;
+	}
+	(*pt)->bal = 0;
+}
+
+void rotateLeft(Node **pt) {
+	Node *ptu = (*pt)->dir;
+	if (!ptu) return;
+	if (ptu->bal == 1) {
+		(*pt)->dir = ptu->esq;
+		ptu->esq = *pt;
+		(*pt)->bal = 0;
+		*pt = ptu;
+	}
+	else {
+		Node *ptv = ptu->esq;
+		ptu->esq = ptv->dir;
+		ptv->dir = ptu;
+		(*pt)->dir = ptv->esq;
+		ptv->esq = *pt;
+		if (ptv && ptv->bal == 1) (*pt)->bal = -1; 
+			else (*pt)->bal = 0;
+		if (ptv && ptv->bal == -1) ptu->bal = 1;
+			else ptu->bal = 0;
+		*pt = ptv;
+	}
+	(*pt)->bal = 0;
+}
+
+//insere um no com chave `chave` na arvore `root` 
+void insert(int chave, Node **pt, int *h){
+	if (!(*pt)) {
+		*pt = create(chave);
+		*h = 1;
+	}
+	if ((*pt)->chave == chave) return;
+	if (chave < (*pt)->chave) {
+		insert(chave, &((*pt)->esq), h);
+		if (*h) {
+			switch ((*pt)->bal) {
+				case 1:
+					(*pt)->bal = 0;
+					*h = 0;
+				break;
+				case 0:
+					(*pt)->bal = -1;
+				break;
+				case -1:
+					rotateRight(pt);
+					*h = 0;
+				break;
+			}
+		} 
+	}
+	else {
+		insert(chave, &((*pt)->dir), h);
+		if (*h) {
+			switch ((*pt)->bal) {
+				case -1:
+					(*pt)->bal = 0;
+					*h = 0;
+				break;
+				case 0:
+					(*pt)->bal = 1;
+				break;
+				case 1:
+					rotateLeft(pt);
+					*h = 0;
+				break;
+		}
+	}
+}
+}
+
+//desaloca um no node e todos seus filhos
+void desalocateTree(Node *node) {
+	if (node->esq)
+		desalocateTree(node->esq);
+	if (node->dir)
+		desalocateTree(node->dir);
+	free(node);
+}
+
+//encontrar o no pai de um no `child` partindo de um no 'node'
+Node *getAncestor(Node *node, Node *child) {
+	Node *parent = NULL;
+	while (node != child) {
+		parent = node;
+		node = node->chave >= child->chave ? node->esq : node->dir;
+	}
+	return parent;
+}
+
+//encontra o valor minimo de uma arvore/subarvore
+Node *getMin(Node *node) {
+	while (node->esq)
+		node = node->esq;
+	return node;
+}
+
+//encontra o valor maximo de uma arvore/subarvore
+Node *getMax(Node *node) {
+	while (node->dir)
+		node = node->dir;
+	return node;
+}
+
+//encontra o sucessor de um no de valor 'chave' partindo de um no `node`
+Node *getSuccessor(Node *node, int chave){
+	Node *successor = NULL;
+	Node *ptr = NULL; //apontar para o no que contem a chave
+	if (!node || search(chave, node, &ptr) != 1)
+		return NULL; //ARVORE nao existe ou entao o valor nao existe na arvore
+	
+	if (ptr->dir) successor = getMin(ptr->dir); //tem subarvore direita
+
+	else while (node != NULL) {
+		if (chave >= node->chave) {
+			node = node->dir;
+		}
+		else {
+			successor = node;
+			node = node->esq;
+		}
+	}
+	return successor;
+}
+
+//encontra o predecessor de um no de valor `chave` partindo de um no `node`
+Node *getPredecessor(Node *node, int chave){
+	Node *predecessor = NULL;
+	Node *ptr = NULL;
+	if (!node || search(chave, node, &ptr) != 1)
+		return NULL; //ARVORE nao existe ou entao o valor nao existe na arvore
+	
+	if (ptr->esq) predecessor = getMax(ptr->esq); //tem subarvore esquerda
+
+	else while (node != NULL) {
+		if (chave <= node->chave) {
+			node = node->esq;
+		}
+		else {
+			predecessor = node;
+			node = node->dir;
+		}
+	}
+	return predecessor;
+}
+
+void checkRotation(Node **pt, int plus) { //plus pra saber se adiciona 1 ou subtrai 1 do bal
+	int bal = plus ? (*pt)->bal + 1 : (*pt)->bal - 1;
+	if (bal == 2) rotateLeft(pt);
+	if (bal == -2) rotateRight(pt);
+
+}
+
+//remove um no `node`
+void removeNode(Node *node, Node **pt, Node *ancestor, int *h) {
+	if (!node) return;
+	if (node != *pt && node->chave < (*pt)->chave) { 
+		removeNode(node, &(*pt)->esq, *pt, h);
+		if (*h) {
+			checkRotation(pt, 1);
+		}
+	}
+	else if (node != *pt && node->chave > (*pt)->chave) {
+		removeNode(node, &(*pt)->dir, *pt, h);
+		if (*h) {
+			checkRotation(pt, 0);
+		}
+	}
+	else {
+		Node *successor = getSuccessor(*pt, node->chave);
+		int isLeftNode;
+		if (ancestor)
+			isLeftNode = ancestor->esq == node; //variavel para saber se `node` eh um filho da direita ou esquerda
+
+		if (!node->esq && !node->dir) { //nao tem subarvores
+			if (ancestor && isLeftNode) //setar como lambda no ancestral
+				ancestor->esq = NULL;
+			else if (ancestor) 
+				ancestor->dir = NULL;
+			*h = 1;
+		}
+
+		else if (!node->esq) {//caso (a) (nao tem filho esquerdo)
+			if (ancestor && isLeftNode) //caso for um filho da esquerda do ancestral, substituir o filho da esquerda do ancestral
+				ancestor->esq = node->dir;
+			else if (ancestor)
+				ancestor->dir = node->dir;
+			*h = 1;
+		}
+
+		else if (node->esq && !node->dir) {//caso (b) (apenas um filho a direita)
+			if (ancestor && isLeftNode)
+				ancestor->esq = node->esq;
+			else if (ancestor)
+				ancestor->dir = node->esq;
+			*h = 1;
+		}
+		
+		else if (node->dir && node->dir == successor) { //caso c
+			if (successor->dir != NULL) *h = 1;
+			successor->bal = node->bal; //herda o balanço e subtrai 1 (pq a subarvore esquerda diminuiu 1)
+			successor->esq = node->esq; //herdar a subarvore esquerda, mesmo se for lambda, successor nunca vai ter subarvore esquerda
+			checkRotation(&successor, 0); // bal -1
+			if (ancestor && ancestor->chave > node->chave) //z eh a chave esquerda do ancestral
+				ancestor->esq = successor;
+			else if (ancestor) ancestor->dir = successor; //chave direita
+		}
+
+		//caso d
+		else {
+			Node *successor_ancestor = getAncestor(*pt	, successor);
+			if (successor_ancestor->dir != NULL) *h = 1;
+			if (successor_ancestor) // SEMPRE o sucessor vai ser um filho da esquerda
+				successor_ancestor->esq = successor->dir; //sucessor NUNCA vai ter uma subarvore esquerda
+			if (ancestor)
+				ancestor->dir = successor;
+			successor->esq = node->esq;
+			successor->dir = node->dir;
+		}
+		free(node);
+	}
+}
+
+//printa a arvore na tela
+void paintNode(Node *node, int x, int y, int depth) {
+	char text[30];
+	int text_w, text_h;
+	if (!node) { //arvore vazia
+		gfx_set_font_size(40);
+		gfx_get_text_size("Arvore Vazia", &text_w, &text_h);
+		gfx_text(WIDTH/2 - text_w/2, HEIGHT/2 - text_h/2, "Arvore Vazia");
+		return;
+	}
+	gfx_set_font_size(10);
+
+	//pintar em pos ordem
+	if (node->esq) { 
+		gfx_line(x, y, x - (200/depth), y + 80);
+		paintNode(node->esq, x - (200/depth), y + 80, depth + 1);
+	}
+	if (node->dir) {
+		gfx_line(x, y, x + (200/depth), y + 80);
+		paintNode(node->dir, x + (200/depth), y + 80, depth + 1);
+	}
+	gfx_set_color(0, 0, 0);
+	gfx_filled_ellipse(x, y, 22, 22);
+	gfx_set_color(255,255,255);
+	gfx_ellipse(x, y, 22, 22);
+	sprintf(text, "%d", node->chave);
+	gfx_get_text_size(text, &text_w, &text_h);
+	gfx_text(x - text_w/2, y - text_h/2, text);
+
+	sprintf(text, "%d", node->bal);
+	gfx_get_text_size(text, &text_w, &text_h);
+	gfx_text(x - text_w/2, y - text_h/2 + 10, text);
+
+}
+
+//funcao recursiva para construir a arvore
+void preorderRead(Node *ancestor, FILE *ptr, int isLeft) {
+	struct s_arq_no *binaryNode = malloc(sizeof(struct s_arq_no));
+	fread(binaryNode, sizeof(struct s_arq_no), 1, ptr);
+
+	Node *newNode = malloc(sizeof(Node)); //alocar o novo no
+	newNode->chave = binaryNode->chave;
+	newNode->esq = NULL;
+	newNode->dir = NULL;
+
+	if (isLeft)	ancestor->esq = newNode;
+	else ancestor->dir = newNode;
+	
+
+	if (binaryNode->esq) {
+		preorderRead(newNode, ptr, 1);
+	}
+	if (binaryNode->dir) {
+		preorderRead(newNode, ptr, 0);
+	}
+	free(binaryNode);
+}
+
+//funcao para abrir o arquivo `filename` e setar a raiz de uma arvore `root`
+void readTree(Node **root, char *filename){
+	FILE *ptr = fopen(filename, "rb");
+	if (!ptr) {
+		printf("[ERRO] Arquivo nao encontrado\n");
+		return;
+	}
+
+	fseek(ptr, 0, SEEK_END); //saber se tem UM no no arquivo (no minimo)
+	size_t size = ftell(ptr);
+	fseek(ptr, 0, SEEK_SET);
+
+	if (sizeof(struct s_arq_no) > size) { //carregar um arquivo vazio significa desalocar a arvore
+		desalocateTree(*root);
+		*root = NULL;
+		printf("[ERRO] Arquivo vazio\n");
+		return;
+	}
+
+	struct s_arq_no *binaryNode = malloc(sizeof(struct s_arq_no));
+	fread(binaryNode, sizeof(struct s_arq_no), 1, ptr);
+
+	//desalocar a raiz antiga (caso exista)
+	if (*root)
+		desalocateTree(*root);
+	
+	*root = malloc(sizeof(Node));
+	*root = create(binaryNode->chave);
+	if (binaryNode->esq)
+		preorderRead(*root, ptr, 1);
+	if (binaryNode->dir)
+		preorderRead(*root, ptr, 0);
+	
+	free(binaryNode);
+	fclose(ptr);
+	printf("[SUCESSO] Arquivo %s carregado\n", filename);
+}
+
+void preorderSave(Node *node, FILE *ptr) {
+	struct s_arq_no *binaryNode = malloc(sizeof(struct s_arq_no));
+	binaryNode->chave = node->chave;
+	binaryNode->esq = node->esq != NULL;
+	binaryNode->dir = node->dir != NULL;
+
+	fwrite(binaryNode, sizeof(struct s_arq_no), 1, ptr);
+	if (node->esq) preorderSave(node->esq, ptr);
+	if (node->dir) preorderSave(node->dir, ptr);
+	free(binaryNode);
+}
+
+void saveTree(Node *root, char *filename){
+
+	FILE *ptr = fopen(filename, "wb+");
+	if (!ptr) return;
+	if (!root) fputc(0, ptr); //arquivo vazio
+	else preorderSave(root, ptr);
+
+
+	fclose(ptr);
+	printf("[SUCESSO] Arquivo %s salvo\n", filename);
+}
+
+void pause(){
+	fflush(stdout);
+	system("read -p \"Pressione uma tecla para continuar\" var");
+}
